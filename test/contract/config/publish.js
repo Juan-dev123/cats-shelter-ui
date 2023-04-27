433577d0a7b22f0644d06d1041c5f5fac2bb38ef
Juan Pablo Ramos Torres <61593415+Juan-dev123@users.noreply.github.com>
2023-04-26 20:28:45 -0500

    post-request (#7)

--- test/contract/config/publish.js
+++ test/contract/config/publish.js
@@ -6,7 +6,8 @@ dotenv.config();
 const opts = {
    pactBroker: process.env.PACT_BROKER_BASE_URL,
    pactBrokerToken: process.env.PACT_BROKER_TOKEN,
-   consumerVersion: process.env.npm_package_version,
+   // consumerVersion: process.env.npm_package_version,
+   consumerVersion: "0.1.1", //cuando agrego nuevas cosas al contrato tengo que ir subiendo la version
    pactFilesOrDirs: ['./test/contract/pacts']
 };
 
