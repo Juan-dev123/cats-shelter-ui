abfe55198f85b3966302e81d255b3aaa32429eca
Juan Pablo Ramos Torres <61593415+Juan-dev123@users.noreply.github.com>
2023-05-14 01:31:59 -0500

    test for delete an animal (#13)

---
+++ test/contract/specs/delete-animals.contract.test.js
@@ -0,0 +1,35 @@
+import { provider } from '../config/init-pact.js';
+import {Matchers} from '@pact-foundation/pact';
+import { AnimalController } from '../../../controllers/AnimalsController.js';
+import { expect } from 'chai';
+import {HttpStatusCode} from "axios";
+
+describe('Animal Service', () => {
+    describe('When a request to delete an animal', () => {
+        before(async () => {
+            await provider.setup();
+            await provider.addInteraction({
+                uponReceiving: 'a request to delete an animal',
+                state: "there are one animal",
+                withRequest: {
+                    method: 'DELETE',
+                    path: '/animals/Manchas',
+                },
+                willRespondWith: {
+                    status: 204
+                }
+            });
+        });
+
+        after(() => provider.finalize());
+
+        it('should return no content', async () => {
+            const response = await AnimalController.delete('Manchas');
+            const responseBody = response.data;
+
+            expect(responseBody).to.be.empty;
+            expect(response.status).to.be.equal(HttpStatusCode.NoContent);
+            await provider.verify()
+        })
+    })
+});
